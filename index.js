1d9fc28c73242cba6f2fd18e5061c172528d8841
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-17 14:11:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event){
+  let thankYouCards = [];
+  for (let i = 0; i < names.length; i++){
+    thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+  }
+  return thankYouCards;
+}
+
+function countdown(countdown) {
+  while (countdown > 0) {
+    console.log(countdown);
+    countdown -= 1;
+  }
+  console.log(countdown);
+}
